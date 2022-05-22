a09306774463083e932cec2281e4d2995cfc7c7f
Matt Aimonetti <113+mattetti@users.noreply.github.com>
2022-05-21 22:34:17 -0700

    fart stream is now on

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/go-audio/audio"
@@ -14,6 +15,11 @@ import (
 	"github.com/gordonklaus/portaudio"
 	"gitlab.com/gomidi/midi/v2"
 	_ "gitlab.com/gomidi/midi/v2/drivers/rtmididrv" // autoregisters driver
+)
+
+var (
+	streamMutex  sync.Mutex
+	stream2Mutex sync.Mutex
 )
 
 func main() {
@@ -54,12 +60,24 @@ func main() {
 		fmt.Println("Not a valid WAV file")
 		os.Exit(1)
 	}
-
 	sampleRate := mooDec.SampleRate
-	format := mooDec.Format()
-	fmt.Println(format.NumChannels, format.SampleRate)
-
 	out := make([]int32, 8192)
+
+	// -------------
+	mooFFart, err := os.Open("sounds/cow_fart_32b.wav")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mooF.Close()
+	mooFartDec := wav.NewDecoder(mooFFart)
+	if !mooFartDec.IsValidFile() { // ReadInfo()
+		fmt.Println("Not a valid cow fart WAV file")
+		os.Exit(1)
+	}
+	fartSampleRate := mooFartDec.SampleRate
+	out2 := make([]int32, 8192)
+
+	// -------------
 	defaultDevice, err := portaudio.DefaultOutputDevice()
 	if err != nil {
 		fmt.Println("Failed to open the default output device:", err)
@@ -67,10 +85,14 @@ func main() {
 	}
 	fmt.Println("Default output device:", defaultDevice.Name)
 
-	fmt.Printf("Opening audio device outputs: %d, sample rate: %d\n", mooDec.NumChans, sampleRate)
+	fmt.Printf("Opening stream outputs: %d, sample rate: %d\n", mooDec.NumChans, sampleRate)
 	stream, err := portaudio.OpenDefaultStream(0, int(mooDec.NumChans), float64(sampleRate), len(out), &out)
 	check(err)
 	defer stream.Close()
+	fmt.Printf("Opening another stream outputs: %d, sample rate: %d\n", mooFartDec.NumChans, fartSampleRate)
+	stream2, err := portaudio.OpenDefaultStream(0, int(mooFartDec.NumChans), float64(fartSampleRate), len(out2), &out2)
+	check(err)
+	defer stream2.Close()
 
 	stop, err := midi.ListenTo(in, func(msg midi.Message, timestampms int32) {
 		var bt []byte
@@ -83,8 +105,16 @@ func main() {
 				os.Exit(0)
 			}
 		case msg.GetNoteStart(&ch, &key, &vel):
-			fmt.Printf("starting note %s on channel %v with velocity %v\n", midi.Note(key), ch, vel)
-			playAudioFile(mooDec, stream, out, &sig)
+
+			fmt.Printf("starting note %d [%s] on channel %v with velocity %v\n", key, midi.Note(key), ch, vel)
+			// Ab3 - Pad 1
+			if key == 44 {
+				go func() { playAudioFile(mooDec, stream, &out, &sig, &streamMutex) }()
+			}
+			// A3 - Pad 2
+			if key == 45 {
+				go func() { playAudioFile(mooFartDec, stream2, &out2, &sig, &stream2Mutex) }()
+			}
 		case msg.GetNoteEnd(&ch, &key):
 			fmt.Printf("ending note %s on channel %v\n", midi.Note(key), ch)
 		default:
@@ -97,28 +127,53 @@ func main() {
 		return
 	}
 
-	time.Sleep(time.Second * 15)
+	for {
+		select {
+		case <-sig:
+			stop()
+			return
+		default:
+		}
+		time.Sleep(150 * time.Millisecond)
+	}
 
-	stop()
 }
 
-func playAudioFile(dec *wav.Decoder, stream *portaudio.Stream, out []int32, sig *chan (os.Signal)) {
+func playAudioFile(dec *wav.Decoder, stream *portaudio.Stream, out *[]int32, sig *chan (os.Signal), mu *sync.Mutex) {
+	fmt.Println("trying to play something")
+	mu.Lock()
+	defer mu.Unlock()
 	check(stream.Start())
 
 	defer stream.Stop()
-	buf := &audio.IntBuffer{Format: dec.Format(), Data: make([]int, len(out))}
+
+	buf := &audio.IntBuffer{Format: dec.Format(), Data: make([]int, len(*out))}
 	n, err := dec.PCMBuffer(buf)
 	//fmt.Println(dec.BitDepth)
 	// Assuming 32bit audio for now
+	streamBuf := *out
 	for i := range buf.Data {
-		out[i] = int32(buf.Data[i])
+		streamBuf[i] = int32(buf.Data[i])
 	}
-	check(stream.Write())
+	if err = stream.Write(); err != nil {
+		fmt.Println(err)
+		time.Sleep(150 * time.Millisecond)
+		if err = stream.Stop(); err != nil {
+			fmt.Println(err)
+			return
+		}
+		stream.Start()
+		if err = stream.Write(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
 	for n > 0 && err == nil {
 		n, err = dec.PCMBuffer(buf)
 		// convert buf to a slice of int32 values
 		for i := range buf.Data {
-			out[i] = int32(buf.Data[i])
+			streamBuf[i] = int32(buf.Data[i])
 		}
 		check(stream.Write())
 		select {
